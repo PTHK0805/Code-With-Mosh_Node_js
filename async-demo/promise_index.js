951bad79de4283e378568d1c23aebf93161ca6bf
PTHK0805 <ptkyaw505@gmail.com>
2022-09-16 14:45:07 +0630

    Async and Await.

--- async-demo/promise_index.js
+++ async-demo/promise_index.js
@@ -1,5 +1,6 @@
 console.log('Before');
 // Callback Hell
+// 1:: Callback approach
 getUser(1, (user) => {
   getRepositories(user.gitHubUsername, (repos) => {
     getCommits(repos[0], (commits) => {
@@ -9,13 +10,36 @@ getUser(1, (user) => {
 });
 
 // Solve Callback Hell with Promise
+
+// 2:: Promise-based approach
 getUser(1)
   .then(user => getRepositories(user))
   .then(repos => getCommits(repos))
   .then(commit => console.log('Commits: ', commit))
   .catch(err => console.log('Error: ', err));
 
+
+// 3:: Async and Await approach
+
+async function displayCommits() {
+
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+
+  catch (err) {
+    console.log('Error : ', err);
+  }
+  
+}
+displayCommits();
+
 console.log('After');
+
+
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
