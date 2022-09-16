1249f6bc48908cedf998dde748b36302339ad68e
PTHK0805 <ptkyaw505@gmail.com>
2022-09-16 10:25:24 +0630

    Replacing callbacks with Promises.

---
+++ async-demo/promise_index.js
@@ -0,0 +1,46 @@
+console.log('Before');
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    })
+  })
+});
+
+console.log('After');
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+
+      resolve({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
+  })
+
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  })
+
+}
+
+function getCommits(repo, callback) {
+  return new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+
+      resolve(['commit']);
+    }, 2000);
+  })
+
+}
