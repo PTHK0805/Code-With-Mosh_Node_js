5eacab42c4292c0d04354ef025ac67212663f267
PTHK0805 <ptkyaw505@gmail.com>
2022-09-16 13:41:50 +0630

    Running Parallel Promise.

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,5 +1,23 @@
 const p = Promise.resolve(1);
-const p1 = Promise.reject(new Error('Reasons for rejection...'));
+//const p3 = Promise.reject(new Error('Reasons for rejection...'));
 
 p.then(result => console.log('Result ', result));
-p1.catch(err => console.log('Error ', err));
+//p3.catch(err => console.log('Error ', err));
+
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Async Operation 1...');
+    resolve(1);
+  }, 2000)
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Async Operation 2...');
+    resolve(2);
+  }, 2000)
+});
+
+Promise.all([p1, p2])
+  .then(result => console.log('Result : ', result))
+  .catch(err => console.log('Error : ', err));
