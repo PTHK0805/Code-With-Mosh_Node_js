4e663d9df6d31343776fc7bee32626203d3f02f3
PTHK0805 <ptkyaw505@gmail.com>
2022-09-19 08:39:32 +0630

    Model.

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -17,4 +17,14 @@ const courseSchema = new mongoose.Schema({
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+const course = new Course({
+  name: 'Node.js Course',
+  author: 'Mosh',
+  tags: ['node', 'backend'],
+  isPublished: true
 })
+
