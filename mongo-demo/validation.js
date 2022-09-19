f5922f72d61013d100cc8a437896e79f6ea0f2e7
PTHK0805 <ptkyaw505@gmail.com>
2022-09-19 15:50:41 +0630

    Built-in Validators.

---
+++ mongo-demo/validation.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.log('Cannot connect to Database...', err));
+
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlenght: 255,
+    //match: /pattern/,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['web', 'mobile', 'network']
+  },
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () { return this.isPublished; }
+  }
+});
+
+const Course = mongoose.model('course', courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: 'Angular Course',
+    category: '-',
+    author: 'Mosh',
+    tags: ['anguular', 'forontend'],
+    isPublished: false,
+    //price: 15
+  })
+
+  try {
+    const result = await course.save();
+    console.log(result);
+  }
+  catch (err) {
+    console.log(err.message);
+  }
+}
+
+createCourse();
