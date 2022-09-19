9a6f8fa478aebeb8184a226e39b537cbecc3d676
PTHK0805 <ptkyaw505@gmail.com>
2022-09-19 10:06:10 +0630

    Querying Document.

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -33,4 +33,13 @@ const result = await course.save();
 console.log(result);
 } 
 
-createCourse();
+async function getCourse() {
+  const courses = await Course
+    .find({ author: 'Mosh', isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+
+getCourse();
